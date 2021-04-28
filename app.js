9cc17e569a007d078025a866d2cf124f5aa4e525
ISHIMWE Eschyle Bersy <ishimweeschylebersy@gmail.com>
2021-04-28 14:17:12 +0200

    working on the winner screen

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ function RandomValue(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+/**
+ * 
+ * 
+ * fonction to tell the winner
+ * @param player value of the player's life
+ * @param monster value of the monster's life
+ * @returns string
+ */
+function checkWinner(player, monster) {
+    if (player <= 0 && monster > 0) {
+        alert("the monster got the better of you this time!");
+    } else if (player > 0 && monster <= 0) {
+        alert("congrats, you win!!!");
+    } else {
+        alert("no winner, both of you lost!");
+    }
+}
+
 const app = Vue.createApp({
     data() {
         return {
@@ -29,16 +47,29 @@ const app = Vue.createApp({
             const attackPower = RandomValue(5, 12);
             this.monsterHealth -= attackPower;
             this.attackPlayer();
+            checkWinner(this.playerHealth, this.monsterHealth);
         },
         attackPlayer() {
             const attackPower = RandomValue(8, 15);
             this.playerHealth -= attackPower;
+            checkWinner(this.playerHealth, this.monsterHealth);
         },
         specialAttack() {
             this.currentRound = 1;
-            const attackPower = RandomValue(15, 25);
+            const attackPower = RandomValue(18, 25);
             this.monsterHealth -= attackPower;
             this.attackPlayer();
+            checkWinner(this.playerHealth, this.monsterHealth);
+        },
+        healPlayer() {
+            const heal = RandomValue(8, 20);
+            if(this.playerHealth >= 80) {
+                alert("huh...you still heathy");
+            } else {
+                this.playerHealth += heal;
+            }
+            this.attackPlayer();
+            checkWinner(this.playerHealth, this.monsterHealth);
         }
     }
 });
